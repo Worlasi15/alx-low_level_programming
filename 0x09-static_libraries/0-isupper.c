b20378a7de6dbf53e1df96fd1210506255728bd5
Worlasi15 <efyawalasi@gmail.com>
2023-05-12 12:34:00 -0700

    creating a script

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -4,11 +4,17 @@
  * _isupper - uppercase letters
  * @c: char to check
  *
- * Return: or 1
+ * Return: 0 or 1
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
