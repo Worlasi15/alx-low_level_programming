cc056189a83d9a85dcc96e0710fc0989da7c54de
Worlasi15 <efyawalasi@gmail.com>
2023-05-25 15:47:44 -0700

    print anything

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg,h>
+/**
+ * print_strings - function that prints strings, followed by a new line
+ * @separator: string to be printed between the strings
+ * @n:  the number of strings passed to the function
+ * @...: A variable number of strings to be printed
+ *
+ * Description: If separator is NULL, it is not printed
+ * If one of the strings if NULL, (nil) is printed instead
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	char *p;
+	unsigned int nl;
+
+	va_start(strings, n);
+
+	for (nl = 0; nl < n; nl++)
+	{
+		p = va_arg(strings, char *);
+
+		if (p == NULL)
+			printf("(nil)");
+		else
+			printf("%s", p);
+
+		if (nl != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(strings);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all -function that prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	int a = 0;
+	char *p, *ptr = "";
+	va_list l;
+
+	va_start(l, format);
+
+	if (format)
+	{
+		while (format[a])
+		{
+			switch (format[a])
+			{
+				case 'c':
+					printf("%s%c", ptr, va_arg(l, int));
+						break;
+				case 'i':
+						printf("%s%d", ptr, va_arg(l, int));
+						break;
+				case 'f':
+						printf("%s%f", p, va_arg(l, double));
+						break;
+				case 's':
+						p = va_arg(l, char *);
+
+						if (!p)
+							p = "(nil)";
+						printf("%s%s", ptr, p);
+						break;
+				default:
+						a++;
+						continue;
+			}
+			p = ", ";
+				a++;
+		}
+	}
+
+	printf("\n");
+	va_end(l);
+}
